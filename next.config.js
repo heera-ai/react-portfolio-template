275aef24d300682678114d7912f98d1ced915176
heera-ai <heera1.lal@ril.com>
2024-05-15 16:32:06 +0530

    added deployment code.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/resume': { page: '/resume' },
+      '/blog': { page: '/blog' },
+      // Add other pages here
+    };
+  }
 }
 
 module.exports = nextConfig
+
+// // next.config.js
+// module.exports = {
+//   trailingSlash: true,
+//   exportPathMap: async function (
+//     defaultPathMap,
+//     { dev, dir, outDir, distDir, buildId }
+//   ) {
+//     return {
+//       '/': { page: '/' },
+//       '/resume': { page: '/resume' },
+//       '/blog': { page: '/blog' },
+//       // Add other pages here
+//     };
+//   },
+// }
