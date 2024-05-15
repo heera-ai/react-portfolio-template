a6b20eeebd09eda266dc8534de864e68f6090157
heera-ai <heera1.lal@ril.com>
2024-05-15 17:42:13 +0530

    removed a tags

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import Socials from "../Socials";
 import Link from "next/link";
 import Button from "../Button";
-
 const Footer = ({}) => {
   return (
     <>
@@ -25,8 +24,8 @@ const Footer = ({}) => {
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
-        <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
+        <Link href="http://www.heeralal.in" className="underline underline-offset-1">
+          Heera Lal
         </Link>
       </h1>
     </>
